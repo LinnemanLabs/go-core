ffcf3373a78ff8b10833b995bc990bc4010320cf
Keith Linneman <keith@linnemanlabs.com>
2026-02-22 23:52:10 -0500

    rename HTTPPort to Port

--- httpserver/config.go
+++ httpserver/config.go
@@ -9,20 +9,20 @@ import (
 // Config adds httpserver-specific configuration fields to the
 // common cfg.Registerable and cfg.Validatable interfaces
 type Config struct {
-	HTTPPort int
+	Port int
 }
 
 // RegisterFlags binds Config fields to the given FlagSet with defaults inline
 func (c *Config) RegisterFlags(fs *flag.FlagSet) {
-	fs.IntVar(&c.HTTPPort, "http-port", 8080, "listen TCP port (1..65535)")
+	fs.IntVar(&c.Port, "http-port", 8080, "listen TCP port (1..65535)")
 }
 
 func (c *Config) Validate() error {
 	var errs []error
 
 	// Ports
-	if c.HTTPPort < 1 || c.HTTPPort > 65535 {
-		errs = append(errs, fmt.Errorf("invalid HTTP_PORT %d (must be 1..65535)", c.HTTPPort))
+	if c.Port < 1 || c.Port > 65535 {
+		errs = append(errs, fmt.Errorf("invalid HTTP_PORT %d (must be 1..65535)", c.Port))
 	}
 
 	if len(errs) > 0 {
